8ffeb235ddb2d74d1405ad9f8f156c424191d45f
Leo Ribas <leonas.ribas@gmail.com>
2021-05-15 18:41:55 -0300

    Integra a página de cadastro de aluno

--- src/pages/unlogged/SignUp/index.jsx
+++ src/pages/unlogged/SignUp/index.jsx
@@ -1,94 +1,170 @@
-import React, { useState } from 'react';
-import { Keyboard, KeyboardAvoidingView, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
-import Button from '../../../components/Button';
-import Input from '../../../components/Input';
-import { PasswordInput, PasswordInputBox } from '../Login/styles';
-import { Container, SubContainer } from './styles';
-import { useNavigation } from '@react-navigation/core';
-import Icon from 'react-native-vector-icons/Entypo';
-import { useEffect } from 'react';
+import React, { useState } from 'react'
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native'
+import Button from '../../../components/Button'
+import Input from '../../../components/Input'
+import * as Styled from '../Login/styles'
+import { Container, SubContainer } from './styles'
+import { useNavigation } from '@react-navigation/core'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import Icon from 'react-native-vector-icons/Entypo'
+import { useEffect } from 'react'
+import { TextInput } from 'react-native-paper'
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
 
-
-
+const signUpSchema = yup.object({
+  name: yup.string().required('Insira seu nome'),
+  email: yup
+    .string()
+    .required('Insira um e-mail.')
+    .email('Insira um e-mail válido.'),
+  phone: yup
+    .number()
+    .typeError('Celular deve ser um número')
+    .required('Insira seu celular'),
+  password: yup
+    .string()
+    .min(6, ({ min }) => `Senha deve conter no mínimo ${min} caracteres`)
+    .required('Insira uma senha.'),
+  password_confirmation: yup
+    .string()
+    .required('Confirme sua senha')
+    .oneOf([yup.ref('password'), null], 'As senhas não são iguais'),
+  authentication: yup.mixed(),
+})
 
 const SignUp = () => {
-  const [isHidden, setIsHidden] = useState(true);
-  const [isHiddenIcon, setIsHiddenIcon] = useState(true);
+  const navigation = useNavigation()
 
-  const [isHidden2, setIsHidden2] = useState(true);
-  const [isHiddenIcon2, setIsHiddenIcon2] = useState(true);
-  const navigation = useNavigation();
-  const [isDisabled, setIsDisabled] = useState(true);
-
-
-  const [isText1, setIsText1] = useState('');
-  const [isText2, setIsText2] = useState('');
-  const [isText3, setIsText3] = useState('');
-  const [isText4, setIsText4] = useState('');
-  const [isText5, setIsText5] = useState('');
-  const [isText6, setIsText6] = useState('');
-
-  useEffect(() => {
-  const handleButton = () => {
-    if ( isText1 && isText2 && isText3 && isText4 && isText5 && isText6)
-    {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true)
-    }
+  const handleSignUp = (values) => {
+    axios
+      .post('http://localhost:3000/users', { user: values })
+      .then(async (response) => {
+        const { data } = response
+        const { token } = data
+        await AsyncStorage.setItem('token', token)
+        navigation.navigate('Feed')
+      })
+      .catch((e) => {
+        console.log(e)
+      })
   }
-  handleButton();
-  })
-  const handleInput = () => {
-    setIsHidden(!isHidden);
-    setIsHiddenIcon(!isHidden);
-  }
-  const handleInput2 = () => {
-    setIsHidden2(!isHidden2);
-    setIsHiddenIcon2(!isHidden2);
-  }
-  return(
-    <KeyboardAvoidingView style={{ flex: 1}}>
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}>
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-
         <Container>
-          <SubContainer>
-            <Input placeholder="Nome Completo" onChangeText={value => setIsText1(value)} />
-            <Input placeholder="Email" onChangeText={value => setIsText2(value)}/>
-            <Input placeholder="Número do celular" onChangeText={value => setIsText3(value)}/>
-            <PasswordInput>
-                <PasswordInputBox placeholder="Senha" secureTextEntry={isHidden} placeholderTextColor="#114978" onChangeText={value => setIsText4(value)} />
-              <TouchableOpacity onPress={handleInput} >
-                {
-                  isHiddenIcon ?
-                  <Icon name="eye" size={20} />
-                  :
-                  <Icon name="eye-with-line" size={20} />
-                }
-              </TouchableOpacity>
-            </PasswordInput>
-            <PasswordInput>
-                <PasswordInputBox placeholder="Confirme sua senha" secureTextEntry={isHidden2} placeholderTextColor="#114978" onChangeText={value => setIsText5(value)} />
-              <TouchableOpacity onPress={handleInput2} >
-                {
-                  isHiddenIcon2 ?
-                  <Icon name="eye" size={20} />
-                  :
-                  <Icon name="eye-with-line" size={20} />
-                }
-              </TouchableOpacity>
-          </PasswordInput>
-            <Input placeholder="ID da Escola" onChangeText={value => setIsText6(value)} />
-            <Button disabled={isDisabled} onPress={() => {
-              navigation.goBack()
-            }}>Cadastrar</Button>
-          </SubContainer>
-
+          <Formik
+            validationSchema={signUpSchema}
+            initialValues={{
+              name: '',
+              email: '',
+              phone: '',
+              password: '',
+              password_confirmation: '',
+            }}
+            onSubmit={(values) => handleSignUp(values)}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+            }) => (
+              <SubContainer>
+                <Styled.TextInputWrapper>
+                  <TextInput
+                    name="name"
+                    mode="outlined"
+                    label="Nome Completo"
+                    value={values.name}
+                    onBlur={handleBlur('name')}
+                    onChangeText={handleChange('name')}
+                    error={errors.name && touched.name}
+                  />
+                  {errors.name && touched.name && (
+                    <Styled.ErrorText>{errors.name}</Styled.ErrorText>
+                  )}
+                </Styled.TextInputWrapper>
+                <Styled.TextInputWrapper>
+                  <TextInput
+                    name="email"
+                    mode="outlined"
+                    label="Email"
+                    value={values.email}
+                    onBlur={handleBlur('email')}
+                    onChangeText={handleChange('email')}
+                    error={errors.email && touched.email}
+                  />
+                  {errors.email && touched.email && (
+                    <Styled.ErrorText>{errors.email}</Styled.ErrorText>
+                  )}
+                </Styled.TextInputWrapper>
+                <Styled.TextInputWrapper>
+                  <TextInput
+                    name="phone"
+                    mode="outlined"
+                    label="Celular"
+                    value={values.phone}
+                    onBlur={handleBlur('phone')}
+                    onChangeText={handleChange('phone')}
+                    error={errors.phone && touched.phone}
+                  />
+                  {errors.phone && touched.phone && (
+                    <Styled.ErrorText>{errors.phone}</Styled.ErrorText>
+                  )}
+                </Styled.TextInputWrapper>
+                <Styled.TextInputWrapper>
+                  <TextInput
+                    name="password"
+                    mode="outlined"
+                    label="Senha"
+                    value={values.password}
+                    onBlur={handleBlur('password')}
+                    onChangeText={handleChange('password')}
+                    secureTextEntry={true}
+                    error={errors.password && touched.password}
+                  />
+                  {errors.password && touched.password && (
+                    <Styled.ErrorText>{errors.password}</Styled.ErrorText>
+                  )}
+                </Styled.TextInputWrapper>
+                <Styled.TextInputWrapper>
+                  <TextInput
+                    name="password_confirmation"
+                    mode="outlined"
+                    label="Confirmação da senha"
+                    value={values.password_confirmation}
+                    onBlur={handleBlur('password_confirmation')}
+                    onChangeText={handleChange('password_confirmation')}
+                    secureTextEntry={true}
+                    error={
+                      errors.password_confirmation &&
+                      touched.password_confirmation
+                    }
+                  />
+                  {errors.password_confirmation &&
+                    touched.password_confirmation && (
+                      <Styled.ErrorText>
+                        {errors.password_confirmation}
+                      </Styled.ErrorText>
+                    )}
+                </Styled.TextInputWrapper>
+                <Button onPress={handleSubmit}>Cadastrar</Button>
+              </SubContainer>
+            )}
+          </Formik>
         </Container>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
-
-  );
+  )
 }
 
-export default SignUp;
+export default SignUp
