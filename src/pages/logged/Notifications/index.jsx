bc4d4f84af13ea53fc1ee3d47cb3cc76b2d770d7
Leo Ribas <leonas.ribas@gmail.com>
2021-05-17 05:36:11 -0300

    Integra página de notificações

--- src/pages/logged/Notifications/index.jsx
+++ src/pages/logged/Notifications/index.jsx
@@ -1,38 +1,24 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import NotificationNotification from '../../../components/Notification';
-import { Container } from './styles';
+import React from 'react'
+import { FlatList } from 'react-native'
+import Notification from '../../../components/Notification'
+import { Container } from './styles'
+import { fetcher } from '../../../services/api'
+import useSWR from 'swr'
 
 const Notifications = () => {
-  const data = [
-    {
-      id: 1,
-      name: 'Paula Amorim',
-      book: 'Quincas Borba',
-    },
-    {
-      id: 2,
-      name: 'Carlos Souza',
-      book: 'Memórias Postumas de Brás Cubas',
-    },
-    {
-      id: 3,
-      name: 'Gustavo Medes',
-      book: 'O cortiço',
-    },
-  ]
-  return (
+  const { data: borrowRequests, error } = useSWR('/borrow_requests', fetcher)
 
+  return (
     <Container>
-    <FlatList
-      data={data}
-      keyExtractor={ item => item.id }
-      renderItem={ ({ item }) => (
-        <NotificationNotification book={item.book} name={item.name} />
-      )}
+      <FlatList
+        data={borrowRequests}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Notification book={item.book} name={item.borrower} />
+        )}
       />
-  </Container>
-  );
+    </Container>
+  )
 }
 
-export default Notifications;
+export default Notifications
