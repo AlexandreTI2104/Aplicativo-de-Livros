a62f41618ee8d35403a7a35f27eebf261cf94308
Leo Ribas <leonas.ribas@gmail.com>
2021-05-18 00:10:46 -0300

    Melhora página de Notificações

--- src/pages/logged/Notifications/index.jsx
+++ src/pages/logged/Notifications/index.jsx
@@ -3,19 +3,30 @@ import { FlatList } from 'react-native'
 import Notification from '../../../components/Notification'
 import { Container } from './styles'
 import { fetcher } from '../../../services/api'
+import { ActivityIndicator, Colors } from 'react-native-paper'
 import useSWR from 'swr'
 
 const Notifications = () => {
   const { data: borrowRequests, error } = useSWR('/borrow_requests', fetcher)
 
-  return (
+  return borrowRequests ? (
     <Container>
       <FlatList
         data={borrowRequests}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <Notification book={item.book} name={item.borrower} />
+          <>
+            <Notification book={item.book} name={item.borrower} />
+          </>
         )}
+      />
+    </Container>
+  ) : (
+    <Container>
+      <ActivityIndicator
+        size={'large'}
+        animating={true}
+        color={Colors.blue500}
       />
     </Container>
   )
