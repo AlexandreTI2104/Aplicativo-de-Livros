33c2670a5d9fd8bfa663e300fc41239bf8692e44
Leo Ribas <leonas.ribas@gmail.com>
2021-05-18 00:09:54 -0300

    Integra criação de livros

--- src/pages/logged/CreateBook/styles.js
+++ src/pages/logged/CreateBook/styles.js
@@ -1,15 +1,10 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
   padding: 20px 20px;
-  justify-content: center;
-  background: white;
 `
-export const SubContainer = styled.View`
-  justify-content: space-between;
-  height: 100%;
-`
+export const SubContainer = styled.View``
 
 export const SwitchInputWrapper = styled.View`
   flex-direction: row;
