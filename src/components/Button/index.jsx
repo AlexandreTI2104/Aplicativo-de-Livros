568bac29b9264b89ea1a58674203021c5fa902b5
Leo Ribas <leonas.ribas@gmail.com>
2021-05-18 03:39:38 -0300

    Formata o componente Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Container, ButtonText } from './styles';
+import React from 'react'
+import { Container, ButtonText } from './styles'
 
 const Button = ({ children, disabled, ...rest }) => {
   return (
-    <Container activeOpacity={0.6} disabled={disabled} isDisabled={disabled} {...rest}>
+    <Container
+      activeOpacity={0.6}
+      disabled={disabled}
+      isDisabled={disabled}
+      {...rest}
+    >
       <ButtonText>{children}</ButtonText>
     </Container>
-  );
+  )
 }
 
-export default Button;
+export default Button
